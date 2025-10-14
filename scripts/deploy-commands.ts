9e7464ef60fe4faa990d87d08451f7a31153da46
Bash <admin@watchthelight.org>
2025-10-14 12:16:38 -0500

    Command deploy repair

--- scripts/deploy-commands.ts
+++ scripts/deploy-commands.ts
@@ -9,6 +9,15 @@ import { env } from "../src/lib/env.js";
 import * as health from "../src/commands/health.js";
 const commands = [health.data.toJSON()];
 async function run() {
+  // eslint-disable-next-line no-console
+  console.log("Token length:", env.DISCORD_TOKEN.length);
+  // eslint-disable-next-line no-console
+  console.log("Token preview:", env.DISCORD_TOKEN.substring(0, 20) + "...");
+  // eslint-disable-next-line no-console
+  console.log("Client ID:", env.CLIENT_ID);
+  // eslint-disable-next-line no-console
+  console.log("Guild ID:", env.GUILD_ID);
+
   const rest = new REST({ version: "10" }).setToken(env.DISCORD_TOKEN);
   if (env.GUILD_ID && env.GUILD_ID.trim().length > 0) {
     await rest.put(Routes.applicationGuildCommands(env.CLIENT_ID, env.GUILD_ID), {
